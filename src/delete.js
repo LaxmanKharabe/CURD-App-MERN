96a67d4948ec03ff5404fab0f98bd38dfc3698ea
LaxmanKharabe <laxman31097@gmail.com>
2024-02-11 23:14:18 +0530

    document will be deleted from the tblCategories collection who has the id 4

--- src/delete.js
+++ src/delete.js
@@ -5,9 +5,9 @@ mongoClient.connect(connectinString)
     let database = clientObj.db("REACT_BACKEND");
     database.collection("tblCategories").deleteOne({CategoryId: 4}).then(()=>{
         console.log(`Record Deleted!`)
+        // document will be deleted from the tblCategories collection who has the id 4
     })
 })
 .catch((errorObj)=>{
     console.log(errorObj);
-    // 
 })
